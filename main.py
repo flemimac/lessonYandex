8c4595b3abc3e1a95838de3363c0f0d4b3f31cb0
flemimac <alex.miazim.2006@gmail.com>
2024-12-03 18:31:25 +0200

    second commit

--- main.py
+++ main.py
@@ -2,31 +2,41 @@ import sys
 import random
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt6.QtGui import QPainter, QColor
 
 
-class Main(QMainWindow):
+class CircleDrawer(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.setWindowTitle("Main")
-        
-        self.pushButton.clicked.connect(self.add_circle)
         self.circles = []
-        
+
     def add_circle(self):
-        diameter = random.randint(20, 150)
+        diameter = random.randint(20, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        for (x, y, diameter) in self.circles:
-            painter.setBrush(QColor(255, 255, 0))
+        for (x, y, diameter, color) in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(x, y, diameter, diameter)
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Circle Drawer")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.circle_drawer = CircleDrawer()
+        self.setCentralWidget(self.circle_drawer)
+
+        self.pushButton = QPushButton("нажми на меня", self)
+        self.pushButton.setGeometry(360, 370, 100, 30)
+        self.pushButton.clicked.connect(self.circle_drawer.add_circle)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
